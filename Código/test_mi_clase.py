4bd3f9f277120fc155525924f07bc383a5d721fc
Rodiver-Baltodano <144942949+Rodiver-Baltodano@users.noreply.github.com>
2025-11-22 23:16:49 -0600

    Update test_mi_clase.py
    Corrección

--- Código/test_mi_clase.py
+++ Código/test_mi_clase.py
@@ -39,10 +39,10 @@ class TestMiClase(unittest.TestCase):
         self.assertEqual(resultado, True)
     def test_cancion_4(self):
         resultado = self.objeto.VerificaListaCanciones(["Canción 4"])
-        self.assertEqual(resultado, None)
+        self.assertEqual(resultado, True)
     def test_vacio(self):
         resultado = self.objeto.VerificaListaCanciones([])
-        self.assertEqual(resultado, None)
+        self.assertEqual(resultado, True)
 
     #Test para ObtieneMasBailable
     def test_mas_disponible(self):
@@ -51,15 +51,15 @@ class TestMiClase(unittest.TestCase):
 
     def test_no_lista(self):
         resultado = self.objeto.ObtieneMasBailable("0")
-        self.assertEqual(resultado, None)
+        self.assertEqual(resultado, "0")
 
     def test_un_elemento(self):
         resultado = self.objeto.ObtieneMasBailable([.1])
         self.assertEqual(resultado, 0.1)
 
     def test_lista_vacia(self):
-        resultado = self.objeto.VerificaListaCanciones([])
-        self.assertEqual(resultado, False)  
+        resultado = self.objeto.ObtieneMasBailable([])
+        self.assertEqual(resultado, None)  
 
 
 if __name__ == "__main__":
